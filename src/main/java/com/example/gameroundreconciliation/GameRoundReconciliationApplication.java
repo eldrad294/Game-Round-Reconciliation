bef647b02505c84dd9e90da83157ebcecbdcdddd
Gabriel Sammut <gabriel.sammut@kindredgroup.com>
2021-09-10 16:30:07 +0200

    Initial Commit

---
+++ src/main/java/com/example/gameroundreconciliation/GameRoundReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameroundreconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRoundReconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRoundReconciliationApplication.class, args);
+	}
+
+}
